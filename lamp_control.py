784a5fb9a510391f46e394b9dfe444b9c09a2ba0
billybob71a <yungpet@gmail.com>
2023-11-29 18:46:53 -0700

    Adding file lamp_control

---
+++ lamp_control.py
@@ -0,0 +1,10 @@
+import RPIO.GPIO as GPIO
+
+GPIO.setWarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(10,GPIO.IN, pull_up_down-GPIO.PUD_DOWN) # set pin 10 to be an input pin and set initial value to be pulled low (off)
+
+while True:
+    if GPIO.input(10) == GPIO.HIGH:
+        print("Button was pushed")
+
