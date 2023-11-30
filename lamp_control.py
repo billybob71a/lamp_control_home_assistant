5f9c1c7175a6a5b3848ed277914654dd1c036a02
billybob71a <yungpet@gmail.com>
2023-11-29 22:25:24 -0700

    trying different boolean

--- lamp_control.py
+++ lamp_control.py
@@ -6,6 +6,6 @@ GPIO.setup(10,GPIO.IN) # set pin 10 to be an input pin and set initial value to 
 
 while True:
     input_value = GPIO.input(10)
-    if input_value == False:
+    if input_value == True:
         print("Who pressed my Button")
 
