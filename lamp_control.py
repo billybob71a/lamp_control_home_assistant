19c4dda854f354416bb9b53d6676dcdbaa6a4e97
billybob71a <yungpet@gmail.com>
2023-11-30 22:29:23 -0700

    Trying to use edd_event_detect function instead

--- lamp_control.py
+++ lamp_control.py
@@ -3,19 +3,26 @@ import RPi.GPIO as GPIO
 
 # GPIO.setWarnings(False)
 GPIO.setmode(GPIO.BOARD)
-button1 = 16
+button1 = 10
+
+def button_callback(channel):
+    print("Button 1 was pushed!")
+
 button2 = 12
 increment = 0
-GPIO.setup(button1,GPIO.IN,pull_up_down=GPIO.PUD_UP) # set pin 16 to be an input pin and set initial value to be pulled low (off)
-GPIO.setup(button2, GPIO.IN,pull_up_down=GPIO.PUD_UP) 
-while True:
-    if GPIO.input(button1) == False:
-        print("Button 1 was pressed")
-        sleep(1)
-    if GPIO.input(button2) == False:
-        print("Button 2 was pressed")
-        sleep(1)
-    increment+=1
+GPIO.setup(button1,GPIO.IN,pull_up_down=GPIO.PUD_DOWN) # set pin 16 to be an input pin and set initial value to be pulled low (off)
+GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback)
+message = input("Press enter to quit\n\n")
+GPIO.cleanup()
+#GPIO.setup(button2, GPIO.IN,pull_up_down=GPIO.PUD_UP) 
+#while True:
+#    if GPIO.input(button1) == False:
+#        print("Button 1 was pressed")
+#        sleep(1)
+#    if GPIO.input(button2) == False:
+#        print("Button 2 was pressed")
+#        sleep(1)
+#    increment+=1
     #print("The value is " + str(increment))
     
 
