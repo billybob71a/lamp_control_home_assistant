4a10dbf3acccde47c2da5a523f29a2952f0baeab
billybob71a <yungpet@gmail.com>
2023-11-30 23:05:33 -0700

    Adding button 2 to be detected

--- lamp_control.py
+++ lamp_control.py
@@ -12,9 +12,14 @@ button2 = 12
 increment = 0
 GPIO.setup(button1,GPIO.IN,pull_up_down=GPIO.PUD_DOWN) # set pin 16 to be an input pin and set initial value to be pulled low (off)
 GPIO.add_event_detect(button1, GPIO.RISING)
+GPIO.add_event_detect(button2, GPIO.RISING)
 while True:
     if GPIO.event_detected(button1):
         print("button 1 pressed")
+        increment+=1
+        print("The value is " + str(increment))
+    elif GPIO.event_detected(button2):
+        print("button 2 was pressed")
         increment+=1
         print("The value is " + str(increment))
 #    if GPIO.input(10) == GPIO.HIGH:
