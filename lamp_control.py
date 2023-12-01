46dc8fc5bce78c1c80e125f2aa41948bb643f7bf
billybob71a <yungpet@gmail.com>
2023-11-30 21:51:22 -0700

    adding increment value to see how many times looped

--- lamp_control.py
+++ lamp_control.py
@@ -3,8 +3,9 @@ import RPi.GPIO as GPIO
 
 # GPIO.setWarnings(False)
 GPIO.setmode(GPIO.BOARD)
-button1=16
-button2=12
+button1 = 16
+button2 = 12
+increment = 0
 GPIO.setup(button1,GPIO.IN,pull_up_down=GPIO.PUD_UP) # set pin 16 to be an input pin and set initial value to be pulled low (off)
 GPIO.setup(button2, GPIO.IN,pull_up_down=GPIO.PUD_UP) 
 while True:
@@ -14,6 +15,9 @@ while True:
     if GPIO.input(button2) == False:
         print("Button 2 was pressed")
         sleep(.1)
+    increment+=1
+    print("The value is " + increment)
+    
 
 
 
