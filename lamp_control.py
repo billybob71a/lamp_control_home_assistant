5171d94ca2c3d96b4d4166f84d466eb3ee64c093
billybob71a <yungpet@gmail.com>
2023-11-30 23:32:40 -0700

    Adding requests to make HTTP POST request to google API

--- lamp_control.py
+++ lamp_control.py
@@ -1,4 +1,6 @@
 from time import sleep
+import json
+import requests
 import RPi.GPIO as GPIO
 
 # GPIO.setWarnings(False)
@@ -11,11 +13,30 @@ GPIO.setup(button1,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(button2,GPIO.IN,pull_up_down=GPIO.PUD_DOWN) # set pin 16 to be an input pin and set initial value to be pulled low (off)
 GPIO.add_event_detect(button1, GPIO.RISING)
 GPIO.add_event_detect(button2, GPIO.RISING)
+url = "http://192.168.1.96:8123/api/services/google_assistant_sdk/send_text_command"
+token = ''
+data = {
+    'command': 'turn on lamp'
+}
+headers = {
+    'Authorization': f'Bearer {token}',
+    'Content-Type': 'application/json'
+}
 while True:
     if GPIO.event_detected(button1):
         print("button 1 pressed")
         increment+=1
         print("The value is " + str(increment))
+        try:
+            response = requests.posts(url, headers=headers, data=json.dumps(data))
+            if response.status_code == 200:
+                print('POST request successful')
+                print("Response:", response.json())
+            else:
+                print(f'Failed with status code : {response.status_code}')
+        except requests.exceptions.RequestException as e:
+            print('Request failed', e)
+                
     elif GPIO.event_detected(button2):
         print("button 2 was pressed")
         increment+=1
