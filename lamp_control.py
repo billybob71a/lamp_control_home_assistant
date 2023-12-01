49bef9ebb093b757539dd3423a563ce84cf8ab4f
billybob71a <yungpet@gmail.com>
2023-11-30 22:32:52 -0700

    using while loop

--- lamp_control.py
+++ lamp_control.py
@@ -11,9 +11,9 @@ def button_callback(channel):
 button2 = 12
 increment = 0
 GPIO.setup(button1,GPIO.IN,pull_up_down=GPIO.PUD_DOWN) # set pin 16 to be an input pin and set initial value to be pulled low (off)
-GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback)
-message = input("Press enter to quit\n\n")
-GPIO.cleanup()
+while True:
+    if GPIO.input(10) == GPIO.HIGH:
+        print("Button 1 was pushed")
 #GPIO.setup(button2, GPIO.IN,pull_up_down=GPIO.PUD_UP) 
 #while True:
 #    if GPIO.input(button1) == False:
