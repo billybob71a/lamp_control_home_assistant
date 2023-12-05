23a652c8421e93a12f05351b3a923bb380c7aee7
billybob71a <yungpet@gmail.com>
2023-12-05 12:02:10 -0700

    modifying lamp control to use callback function using one pin for testing for the time being

--- lamp_control.py
+++ lamp_control.py
@@ -9,13 +9,13 @@ logging.basicConfig(filename='lamp_control.log', level=logging.INFO, format='%(a
 # GPIO.setWarnings(False)
 GPIO.setmode(GPIO.BOARD)
 button1 = 16
-button2 = 40
+#button2 = 40
 
 increment = 0
 GPIO.setup(button1,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(button2,GPIO.IN,pull_up_down=GPIO.PUD_DOWN) # set pin 16 to be an input pin and set initial value to be pulled low (off)
-GPIO.add_event_detect(button1, GPIO.RISING)
-GPIO.add_event_detect(button2, GPIO.RISING)
+#GPIO.setup(button2,GPIO.IN,pull_up_down=GPIO.PUD_DOWN) # set pin 16 to be an input pin and set initial value to be pulled low (off)
+GPIO.add_event_detect(button1, GPIO.RISING, callback = lambda button1: call_api(button1), bouncetime=500)
+#GPIO.add_event_detect(button2, GPIO.RISING)
 url = "http://192.168.1.96:8123/api/services/google_assistant_sdk/send_text_command"
 # the next lines are to read a the secret token from a file
 file_path = 'secret.txt'  # Replace with the path to your file
@@ -40,14 +40,14 @@ headers = {
     'Content-Type': 'application/json'
 }
 
-def call_api(onoff):
-    if onoff == 'on':
-        data = {
-            'command': 'turn on lamp'
-        }
-    elif onoff == 'off':
+def call_api(pin):
+    if pin == 16:
         data = {
             'command': 'turn off lamp'
+        }
+    elif pin == 'off':
+        data = {
+            'command': 'turn on lamp'
         }
     try:
         response = requests.post(url, headers=headers, data=json.dumps(data))
@@ -65,12 +65,12 @@ try:
             logging.info("on button was pressed")
             increment+=1
             logging.info("The value is " + str(increment))
-            call_api('on')                
-        elif GPIO.event_detected(button2):
-            logging.info("off button pressed")
-            increment+=1
-            logging.info("The value is " + str(increment))
-            call_api('off')
+            #call_api('on')                
+        #elif GPIO.event_detected(button2):
+        #    logging.info("off button pressed")
+        #    increment+=1
+        #    logging.info("The value is " + str(increment))
+            #call_api('off')
         sleep(.1)
 except KeyboardInterrupt as e:
     GPIO.cleanup()
